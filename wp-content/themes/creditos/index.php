56ff4cba422d5abc05339d08150d9bf39daf4d8f
Emmanuel Tuffet <dontuffet@gmail.com>
2026-08-24 11:46:13 -0400

    Add CreditOS theme index

---
+++ wp-content/themes/creditos/index.php
@@ -0,0 +1,13 @@
+<?php
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+get_header();
+?>
+<main class="creditos-main">
+    <div class="creditos-card">
+        <p><?php esc_html_e( 'CreditOS theme is active.', 'creditos' ); ?></p>
+    </div>
+</main>
+<?php
+get_footer();
