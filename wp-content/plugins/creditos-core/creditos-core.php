677b3a642a0c01f75e96636fac8e406ddb1d5ddc
Emmanuel Tuffet <dontuffet@gmail.com>
2026-09-03 19:58:50 -0400

    feat: upgrade CreditOS core bootstrap to v0.2.0

--- wp-content/plugins/creditos-core/creditos-core.php
+++ wp-content/plugins/creditos-core/creditos-core.php
@@ -3,7 +3,7 @@
  * Plugin Name: CreditOS Core
  * Plugin URI: https://creditos.legacyxfirm.us
  * Description: Core application services for Legacy X Firm Credit Operating Solutions (CreditOS).
- * Version: 0.1.0
+ * Version: 0.2.0
  * Author: Legacy X Firm
  * Text Domain: creditos
  */
@@ -12,23 +12,23 @@ if ( ! defined( 'ABSPATH' ) ) {
     exit;
 }
 
-define( 'CREDITOS_CORE_VERSION', '0.1.0' );
+define( 'CREDITOS_CORE_VERSION', '0.2.0' );
 define( 'CREDITOS_CORE_FILE', __FILE__ );
 define( 'CREDITOS_CORE_DIR', plugin_dir_path( __FILE__ ) );
 define( 'CREDITOS_CORE_URL', plugin_dir_url( __FILE__ ) );
 
 require_once CREDITOS_CORE_DIR . 'includes/class-creditos-activator.php';
+require_once CREDITOS_CORE_DIR . 'includes/class-creditos-repository.php';
+require_once CREDITOS_CORE_DIR . 'includes/class-creditos-rest.php';
 require_once CREDITOS_CORE_DIR . 'includes/class-creditos-core.php';
 
 register_activation_hook( __FILE__, array( 'CreditOS_Activator', 'activate' ) );
 
 function creditos_core() {
     static $instance = null;
-
     if ( null === $instance ) {
         $instance = new CreditOS_Core();
     }
-
     return $instance;
 }
 
