d57f0e9de8ee838a4db3f2d5d77fc37cf217972a
Emmanuel Tuffet <dontuffet@gmail.com>
2026-09-03 20:05:24 -0400

    fix: keep CreditOS role capabilities synchronized

--- wp-content/plugins/creditos-core/includes/class-creditos-core.php
+++ wp-content/plugins/creditos-core/includes/class-creditos-core.php
@@ -10,6 +10,7 @@ class CreditOS_Core {
 
     public function __construct() {
         $this->maybe_upgrade();
+        $this->ensure_role_capabilities();
         $this->repository = new CreditOS_Repository();
         $this->rest = new CreditOS_REST( $this->repository );
         add_action( 'admin_menu', array( $this, 'register_admin_menu' ) );
@@ -18,6 +19,22 @@ class CreditOS_Core {
     public function maybe_upgrade() {
         if ( get_option( 'creditos_db_version' ) !== CREDITOS_CORE_VERSION ) {
             CreditOS_Activator::activate();
+        }
+    }
+
+    private function ensure_role_capabilities() {
+        $caps = array(
+            'creditos_client' => array( 'creditos_view_own_profile', 'creditos_manage_own_onboarding', 'creditos_manage_own_documents' ),
+            'creditos_specialist' => array( 'creditos_view_own_profile', 'creditos_manage_own_onboarding', 'creditos_manage_own_documents', 'creditos_manage_clients', 'creditos_manage_roadmaps', 'creditos_manage_tasks', 'creditos_manage_disputes', 'creditos_view_staff_dashboard' ),
+            'creditos_business_specialist' => array( 'creditos_view_own_profile', 'creditos_manage_own_onboarding', 'creditos_manage_own_documents', 'creditos_manage_clients', 'creditos_manage_roadmaps', 'creditos_manage_tasks', 'creditos_manage_disputes', 'creditos_view_staff_dashboard', 'creditos_manage_business_credit' ),
+            'administrator' => array( 'creditos_view_own_profile', 'creditos_manage_own_onboarding', 'creditos_manage_own_documents', 'creditos_manage_clients', 'creditos_manage_roadmaps', 'creditos_manage_tasks', 'creditos_manage_disputes', 'creditos_view_staff_dashboard', 'creditos_manage_business_credit' ),
+        );
+        foreach ( $caps as $role_name => $role_caps ) {
+            $role = get_role( $role_name );
+            if ( ! $role ) continue;
+            foreach ( $role_caps as $cap ) {
+                if ( ! $role->has_cap( $cap ) ) $role->add_cap( $cap );
+            }
         }
     }
 
